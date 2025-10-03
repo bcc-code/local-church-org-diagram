d01834c9628cdef39b580faf2b95133f1284b71f
Jacob Eisenberg <jacob@creave.dk>
2025-10-03 14:15:25 +0200

    Retrieve groups from supabase

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,33 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
+from supabase import Client, create_client
+
+load_dotenv()
 
 app = Flask(__name__)
+supabase: Client = create_client(os.environ["SUPABASE_URL"], os.environ["SUPABASE_KEY"])
+supabase.options.schema = "public"
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "OK!"
 
-@app.route('/tree', methods=['GET'])
+
+@app.route("/tree", methods=["GET"])
 def get_tree():
+    groups = supabase.table("groups").select("*").execute().data
     return [
-        {"id": 1, "name": "Root", "parent_id": None},
-        {"id": 2, "name": "Child 1", "parent_id": 1},
-        {"id": 3, "name": "Child 2", "parent_id": 1}
+        {
+            "group_id": group["id"],
+            "label": group["name"],
+            "parent_group_id": group["parent_id"],
+        }
+        for group in groups
     ]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
