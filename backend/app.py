87589b7cb4c2b4a333b47914079252a52e3a18bc
Jacob Eisenberg <jacob@creave.dk>
2025-10-03 13:41:04 +0200

    Add /tree endpoint

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask!"
+    return "OK!"
+
+@app.route('/tree', methods=['GET'])
+def get_tree():
+    return [
+        {"id": 1, "name": "Root", "parent_id": None},
+        {"id": 2, "name": "Child 1", "parent_id": 1},
+        {"id": 3, "name": "Child 2", "parent_id": 1}
+    ]
 
 if __name__ == '__main__':
     app.run(debug=True)
