c53e99804d3789a4dd5659ed748c4a9ca1b6482b
fredrikgangso <fredrik@gangso.dev>
2025-10-25 13:30:51 +0200

    Add close button to GroupMembersDialog and update Button component for extra small size

--- frontend/src/components/GroupMembersDialog.vue
+++ frontend/src/components/GroupMembersDialog.vue
@@ -1,6 +1,12 @@
 <template>
     <Dialog v-model:open="isOpen">
         <DialogContent class="max-w-md bg-neutral-0 border-neutral-200">
+            <DialogClose as-child class="absolute right-3 top-2">
+                <Button size="sm"
+                    class="bg-red-500 text-white hover:text-white hover:bg-red-600 opacity-80 hover:opacity-100">
+                    X
+                </Button>
+            </DialogClose>
             <DialogHeader>
                 <DialogTitle class="text-heading-lg text-neutral-900">{{ groupName }} - Medlemmer</DialogTitle>
                 <DialogDescription class="text-body-sm text-neutral-600">
@@ -13,13 +19,6 @@
             <div v-if="!isLoading" class="max-h-64 overflow-y-auto space-y-2 py-2">
                 <MemberCard v-for="member in groupMembers" :key="member.person_uid" :member="member" />
             </div>
-
-            <DialogFooter>
-                <DialogClose as-child>
-                    <Button variant="secondary"
-                        class="bg-neutral-100 text-neutral-900 hover:bg-neutral-200">Lukk</Button>
-                </DialogClose>
-            </DialogFooter>
         </DialogContent>
     </Dialog>
 </template>
