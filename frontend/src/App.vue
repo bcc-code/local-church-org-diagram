a8091e7473588a00ce18c5a72b06d2fcbcf2217a
fredrikgangso <fredrik@gangso.dev>
2025-10-31 16:57:11 +0100

    feat: remove app title from App.vue template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-6 h-screen max-h-screen bg-brand-600">
-    <h1 class="text-title-lg mb-6 text-neutral-0">{{ TEXTS.APP_TITLE }}</h1>
     <OrgChart />
   </div>
 </template>
