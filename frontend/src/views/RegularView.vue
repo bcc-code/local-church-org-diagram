c803f354897f3673bc7977bffd9a383aa6b2cc22
fredrikgangso <fredrik@gangso.dev>
2025-11-07 09:38:06 +0100

    Refactor AdminView and RegularView to remove unused title elements and imports

--- frontend/src/views/RegularView.vue
+++ frontend/src/views/RegularView.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="p-6 h-screen max-h-screen bg-brand-600">
-    <h1 class="text-title-lg mb-6 text-neutral-0">{{ TEXTS.APP_TITLE }}</h1>
     <OrgChart />
   </div>
 </template>
 
 <script lang="ts" setup>
 import OrgChart from '../components/OrgChart.vue';
-import { TEXTS } from '../constants';
 </script>
